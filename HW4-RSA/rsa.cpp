7053b9c357f1df8d5562433f26c687eb95fc8f33
Sean Wei <me@sean.taipei>
2021-04-22 00:21:05 +0800

    Add Homework #4 spec

---
+++ HW4-RSA/rsa.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cryptopp/rsa.h>
+
+/*
+ * RSA Encryption
+ *
+ * Introduction to Cryptography  Homework #4
+ *
+ * Source Code: https://git.io/RSA
+ *
+ * 2021-04-22 by Sean, 0816146 韋詠祥
+ */
+
+using namespace std;
+using namespace CryptoPP;
+
+int main() {
+	cout << "Loren ipsum.\n";
+
+	return 0;
+}
